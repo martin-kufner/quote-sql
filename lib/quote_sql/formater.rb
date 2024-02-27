103b45e19d45effb5bc807f52350ebe6cfcb3106
mk <martin.kufner@quiz.baby>
2024-02-27 16:58:51 +0100

    neues inject

--- lib/quote_sql/formater.rb
+++ lib/quote_sql/formater.rb
@@ -1,3 +1,4 @@
+require 'niceql'
 class QuoteSql
   module Formater
     PG_FORMAT_BIN = `which pg_format`.chomp.presence
@@ -9,15 +10,14 @@ class QuoteSql
 
     def to_formatted_sql
       sql = respond_to?(:to_sql) ? to_sql : to_s
-      Niceql::Prettifier.prettify_sql(sql)
+      Niceql::Prettifier.prettify_sql(sql.gsub(/(?<=[^%])%(?=\S)/, "%%"))
 
       # IO.popen(PG_FORMAT_BIN, "r+", err: "/dev/null") do |f|
       #   f.write(sql)
       #   f.close_write
       #   f.read
       # end
-    rescue
-      sql
+
     end
 
     alias to_sqf to_formatted_sql
